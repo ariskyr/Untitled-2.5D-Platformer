13226e65af3f3033736f58bc2bd6d5f21e7cb2c0
jimbouzi <58567493+jimbouzi@users.noreply.github.com>
2024-04-02 20:12:10 +0300

    Adding Attack State structure (#19)

---
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerAttackState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackState : PlayerAbilityState
+{
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        player.SetVelocityXZ(Vector2.zero);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        //player.SetVelocityXZ(Vector2.zero);
+
+    }
+    public override void AnimationFinishedTrigger()
+    {
+        base.AnimationFinishedTrigger();
+
+        isAbilityDone = true;
+    }
+}
